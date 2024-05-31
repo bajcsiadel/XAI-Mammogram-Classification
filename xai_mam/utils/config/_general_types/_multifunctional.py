1bea43afab11f1895c6adf6ae02a7af627ec85cc
Adél Bajcsi <33290209+bajcsiadel@users.noreply.github.com>
2024-05-31 13:07:03 +0200

    moved GPU device definition from .env to config (#23)

--- xai_mam/utils/config/_general_types/_multifunctional.py
+++ xai_mam/utils/config/_general_types/_multifunctional.py
@@ -4,6 +4,8 @@ __all__ = ["BatchSize", "Gpu"]
 
 import platform
 
+import torch
+
 
 @dc.dataclass
 class BatchSize:
@@ -21,6 +23,7 @@ class BatchSize:
 class Gpu:
     disabled: bool = False
     device: str = "cuda" if platform.system() != "Darwin" else "mps"
+    device_ids: str = ""
 
     def __setattr__(self, key, value):
         match key:
@@ -37,3 +40,21 @@ class Gpu:
                     self.device = "cpu"
 
         super().__setattr__(key, value)
+
+    def __post_init__(self):
+        match self.device:
+            case "cuda":
+                if not torch.cuda.is_available():
+                    raise ValueError("CUDA is not available.")
+                if self.device_ids:
+                    for device_id in self.device_ids.split(","):
+                        if not 0 <= int(device_id) < torch.cuda.device_count():
+                            raise ValueError(
+                                f"Device {device_id} is not available. There "
+                                f"are {torch.cuda.device_count()} devices."
+                            )
+                else:
+                    self.device_ids = f"{torch.cuda.current_device()}"
+            case "mps":
+                if not torch.backends.mps.is_available():
+                    raise ValueError("MPS is not available.")
