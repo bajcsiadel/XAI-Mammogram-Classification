24ad93baac810441edb1518ce9fd6f28ae3f8837
Adél Bajcsi <bajcsidel96@gmail.com>
2024-07-03 15:40:32 +0300

    added script to train torchvision resnet on any dataset

--- xai_mam/scripts/test_models/train_torchvision_resnet.py
+++ xai_mam/scripts/test_models/train_torchvision_resnet.py
@@ -9,6 +9,7 @@ import numpy as np
 import random
 
 from dotenv import load_dotenv
+from torch.utils.data import SubsetRandomSampler
 from torchinfo import summary
 from tqdm import tqdm
 from torchvision import models
@@ -137,104 +138,136 @@ def main(cfg: main_cfg.Config):
 
 def run(cfg, logger):
     data_module = hydra.utils.instantiate(cfg.data.datamodule)
-    logger.log_data_module(data_module)
     epochs = cfg.model.phases["joint"].epochs
-    batch_size = 16
-    learning_rate = 0.01
+    batch_size = cfg.model.phases["joint"].batch_size.train
+    learning_rate = cfg.model.phases["joint"].learning_rates["features"]
     device = torch.device(
         f"{cfg.gpu.device}" +
         f":{cfg.gpu.device_ids[0]}" if cfg.gpu.device == "cuda" else ""
     )
 
-    train_loader = data_module.train_dataloader(batch_size=batch_size)
-    validation_loader = data_module.validation_dataloader(batch_size=batch_size)
-
-    if not hasattr(models, cfg.model.network.name):
-        raise ValueError(
-            f"Model {cfg.model.network.name!r} not implemented in torchvision."
+    for fold, (train_sampler, validation_sampler) in [(1, (SubsetRandomSampler(np.arange(len(data_module.train_data))), SubsetRandomSampler(np.arange(len(data_module.test_data)))))]:
+        train_loader = data_module.train_dataloader(
+            batch_size=batch_size,
+            sampler=train_sampler,
+        )
+        validation_loader = data_module.test_dataloader(
+            batch_size=batch_size,
+            sampler=validation_sampler,
+        )
+        logger.log_dataloader_information(
+            (f"fold {fold} train dataloader", train_loader),
+            (f"fold {fold} validation dataloader", validation_loader),
         )
 
-    weights = [
-        weight
-        for weight in dir(models)
-        if weight.lower() == f"{cfg.model.network.name}_weights"
-    ]
-    if not weights:
-        raise ValueError(
-            f"Weights for model {cfg.model.network.name!r} not "
-            f"implemented in torchvision."
+        if not hasattr(models, cfg.model.network.name):
+            raise ValueError(
+                f"Model {cfg.model.network.name!r} not implemented in torchvision."
+            )
+
+        weights = [
+            weight
+            for weight in dir(models)
+            if weight.lower() == f"{cfg.model.network.name}_weights"
+        ]
+        if not weights:
+            raise ValueError(
+                f"Weights for model {cfg.model.network.name!r} not "
+                f"implemented in torchvision."
+            )
+        else:
+            weights = getattr(models, weights[0]).DEFAULT
+        model = getattr(models, cfg.model.network.name)(weights=weights)
+        if cfg.data.set.image_properties.n_color_channels != 3:
+            model.conv1 = nn.Conv2d(
+                1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+            )
+        model.fc = nn.Linear(
+            model.fc.in_features, data_module.dataset.number_of_classes
         )
-    else:
-        weights = getattr(models, weights[0]).DEFAULT
-    model = getattr(models, cfg.model.network.name)(weights=weights)
-    if cfg.data.set.image_properties.n_color_channels != 3:
-        model.conv1 = nn.Conv2d(
-            1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
-        )
-    model.fc = nn.Linear(model.fc.in_features, data_module.dataset.number_of_classes)
-    model.to(device)
+        model.to(device)
 
-    logger.info(model)
-    exit()
-    logger.info(summary(
-        model,
-        input_size=(
-            data_module.dataset.image_properties.n_color_channels,
-            data_module.dataset.image_properties.height,
-            data_module.dataset.image_properties.width,
-        ),
-        depth=5,
-        batch_dim=0,
-        device=device,
-        verbose=0,
-    ))
-
-    # Optimizer.
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-    # Loss function.
-    criterion = nn.CrossEntropyLoss()
-
-    # Start the training.
-    for epoch in range(epochs):
-        logger.info(f"Epoch {epoch + 1} of {epochs}")
-        train_epoch_loss, train_epoch_acc = train(
+        logger.info(summary(
             model,
-            train_loader,
-            optimizer,
+            input_size=(
+                data_module.dataset.image_properties.n_color_channels,
+                data_module.dataset.image_properties.height,
+                data_module.dataset.image_properties.width,
+            ),
+            depth=5,
+            batch_dim=0,
+            device=device,
+            verbose=0,
+        ))
+
+        # Optimizer.
+        optimizer = hydra.utils.instantiate(
+            cfg.model.phases["joint"].optimizer, model.parameters(), learning_rate
+        )
+        scheduler = hydra.utils.instantiate(
+            cfg.model.phases["joint"].scheduler, optimizer
+        )
+        # Loss function.
+        criterion = nn.CrossEntropyLoss()
+
+        # Start the training.
+        for epoch in range(epochs):
+            logger.info(f"Epoch {epoch + 1} of {epochs}")
+            train_epoch_loss, train_epoch_acc = train(
+                model,
+                train_loader,
+                optimizer,
+                criterion,
+                device,
+                logger,
+            )
+
+            scheduler.step()
+
+            valid_epoch_loss, valid_epoch_acc = validate(
+                model,
+                validation_loader,
+                criterion,
+                device,
+                logger,
+            )
+            logger.info(
+                f"Training loss: {train_epoch_loss:.3f}, "
+                f"training acc: {train_epoch_acc:.3%}"
+            )
+            logger.info(f"L1 = {model.fc.weight.norm(p=1).item()}")
+            logger.info(
+                f"Validation loss: {valid_epoch_loss:.3f}, "
+                f"validation acc: {valid_epoch_acc:.3%}"
+            )
+            logger.tensorboard.add_scalar("loss/train", train_epoch_loss, epoch)
+            logger.tensorboard.add_scalar("loss/eval", valid_epoch_loss, epoch)
+            logger.tensorboard.add_scalars("loss", {
+                "loss/train": train_epoch_loss,
+                "loss/eval": valid_epoch_loss,
+            }, epoch)
+            logger.tensorboard.add_scalar("accuracy/train", train_epoch_acc, epoch)
+            logger.tensorboard.add_scalar("accuracy/eval", valid_epoch_acc, epoch)
+            logger.tensorboard.add_scalars("loss", {
+                "accuracy/train": train_epoch_acc,
+                "accuracy/eval": valid_epoch_acc,
+            }, epoch)
+            logger.info("-" * 50)
+
+        logger.info("TRAINING COMPLETE")
+
+        test_loss, test_acc = validate(
+            model,
+            data_module.test_dataloader(batch_size=batch_size),
             criterion,
             device,
             logger,
         )
-        valid_epoch_loss, valid_epoch_acc = validate(
-            model,
-            validation_loader,
-            criterion,
-            device,
-            logger,
+        logger.info(f"Test loss: {test_loss:.3f}, test acc: {test_acc:.3%}")
+        logger.tensorboard.add_text(
+            "Test results",
+            f"Loss: {test_loss:.3f}\nAccuracy: {test_acc:.3%}",
         )
-        logger.info(
-            f"Training loss: {train_epoch_loss:.3f}, "
-            f"training acc: {train_epoch_acc:.3%}"
-        )
-        logger.info(
-            f"Validation loss: {valid_epoch_loss:.3f}, "
-            f"validation acc: {valid_epoch_acc:.3%}"
-        )
-        logger.tensorboard.add_scalar("loss/train", train_epoch_loss, epoch)
-        logger.tensorboard.add_scalar("loss/eval", valid_epoch_loss, epoch)
-        logger.tensorboard.add_scalars("loss", {
-            "loss/train": train_epoch_loss,
-            "loss/eval": valid_epoch_loss,
-        }, epoch)
-        logger.tensorboard.add_scalar("accuracy/train", train_epoch_acc, epoch)
-        logger.tensorboard.add_scalar("accuracy/eval", valid_epoch_acc, epoch)
-        logger.tensorboard.add_scalars("loss", {
-            "accuracy/train": train_epoch_acc,
-            "accuracy/eval": valid_epoch_acc,
-        }, epoch)
-        logger.info("-" * 50)
-
-    print("TRAINING COMPLETE")
 
 
 if __name__ == "__main__":
